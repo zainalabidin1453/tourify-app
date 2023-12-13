403b1a579ce28c2a7c8088f43e9fcdc27eda7422
Anjeli Gusnawan <116627857+Anjeellun@users.noreply.github.com>
2023-12-14 02:17:51 +0700

    Update TopBarAllWisata.kt
    -> unused import

--- app/src/main/java/com/tourify/tourifyapp/ui/component/TopBarAllWisata.kt
+++ app/src/main/java/com/tourify/tourifyapp/ui/component/TopBarAllWisata.kt
@@ -9,10 +9,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
