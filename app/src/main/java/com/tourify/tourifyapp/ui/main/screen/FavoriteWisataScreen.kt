9cf483ff8e3c8b84f1ccb0755a93d5096ffb3ec2
Anjeli Gusnawan <116627857+Anjeellun@users.noreply.github.com>
2023-12-14 04:14:49 +0700

    Update FavoriteWisataScreen.kt
    > Unused import
    -> title = stringResource(id = R.string.favorite_list)

--- app/src/main/java/com/tourify/tourifyapp/ui/main/screen/FavoriteWisataScreen.kt
+++ app/src/main/java/com/tourify/tourifyapp/ui/main/screen/FavoriteWisataScreen.kt
@@ -3,13 +3,13 @@ package com.tourify.tourifyapp.ui.main.screen
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.tourify.tourifyapp.R
 import com.tourify.tourifyapp.ui.component.TopBarScreen
-import com.tourify.tourifyapp.ui.component.TopBarScreenModal
 
 @Composable
 fun FavoriteWisataScreen(
@@ -18,7 +18,7 @@ fun FavoriteWisataScreen(
     Scaffold (
         topBar = {
             TopBarScreen(
-                title = "Daftar Favorit",
+                title = stringResource(id = R.string.favorite_list),
                 onBack = {
                     onBack()
                 }
